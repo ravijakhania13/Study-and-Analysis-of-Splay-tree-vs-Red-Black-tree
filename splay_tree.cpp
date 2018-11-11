4decaaf67a35f5f9fce8de54268252c5c10dc5af
Bhavi Dhingra <bhavi.dhingra@students.iiit.ac.in>
2018-11-11 10:39:10 +0530

    using standard notations in splay tree implementation - splay(), join(), split()

--- splay_tree.cpp
+++ splay_tree.cpp
@@ -14,7 +14,9 @@ class SplayTree
 {
     SplayNode *root;
 
-    SplayNode* searchUtil(SplayNode*, int);
+    SplayNode*                    splay(SplayNode*, int);
+    SplayNode*                    join (SplayNode*, SplayNode*);
+    pair<SplayNode*, SplayNode*>  split(SplayNode*, int);
     void preorderUtil(SplayNode*);
     void inorderUtil(SplayNode*);
     void clearUtil(SplayNode*);
@@ -32,9 +34,11 @@ public:
     void insert(int);
     bool search(int);
     void remove(int);
+
     void preorder() { preorderUtil(root); cout << endl; }
     void inorder() { inorderUtil(root); cout << endl; }
 
+    bool isEmpty() { return root == NULL; }
     void clear() { clearUtil(root); root = NULL; }
 
     ~SplayTree()
@@ -98,7 +102,7 @@ SplayNode* SplayTree::zigzag(SplayNode *node)
     return zag(node);
 }
 
-SplayNode* SplayTree::searchUtil(SplayNode* node,int val)
+SplayNode* SplayTree::splay(SplayNode* node,int val)
 {
     // Base cases: node is NULL or val is present at node
     if(!node || node->value == val)
@@ -117,13 +121,13 @@ SplayNode* SplayTree::searchUtil(SplayNode* node,int val)
         if(node->left->value > val)
         {
             // recursively bring val as root of left-left
-            node->left->left = searchUtil (node->left->left, val);
+            node->left->left = splay (node->left->left, val);
             return (node->left->left) ? zigzig(node) : zig(node);
         }
 
         // Zag-Zig (Left Right)
         // recursively bring val as root of left-right
-        node->left->right = searchUtil (node->left->right, val);
+        node->left->right = splay (node->left->right, val);
 
         return (node->left->right) ? zagzig(node) : zig(node);
     }
@@ -139,22 +143,51 @@ SplayNode* SplayTree::searchUtil(SplayNode* node,int val)
         if(node->right->value < val)
         {
             // recursively bring val as root of right-right
-            node->right->right = searchUtil (node->right->right, val);
+            node->right->right = splay (node->right->right, val);
             return (node->right->right) ? zagzag(node) : zag(node);
         }
 
         // Zig-Zag (Right Left)
         // recursively bring val as root of right-left
-        node->right->left = searchUtil (node->right->left, val);
+        node->right->left = splay (node->right->left, val);
 
         return (node->right->left) ? zigzag(node) : zag(node);
     }
 }
 
 
+SplayNode* SplayTree::join(SplayNode* left, SplayNode* right)
+{
+    if(!left) return right;
+    if(!right) return left;
+    left = splay(left, right->value);
+    left->right = right;
+    return left;
+}
+
+
+// left <= val, val < right
+pair<SplayNode*, SplayNode*> SplayTree::split(SplayNode* x, int val)
+{
+    if(!x) return {NULL, NULL};
+    x = splay(x, val);
+    if(x->value <= val)
+    {
+        SplayNode* right = x->right;
+        x->right = NULL;
+        return {x, right};
+    }
+    SplayNode* left = x->left;
+    x->left = NULL;
+    return {left, x};
+}
+
+
 bool SplayTree::search(int val)
 {
-    root = searchUtil(root, val);
+    if(isEmpty()) return false;
+
+    root = splay(root, val);
     if (root->value != val)
     {
         cout << val << " not found in the SplayTree\n";
@@ -166,33 +199,21 @@ bool SplayTree::search(int val)
 
 void SplayTree::insert(int val)
 {
-    SplayNode* new_node = NULL;
+    pair<SplayNode*, SplayNode*> p;
 
-    if(!root)
+    if(isEmpty())
     {
         root = new SplayNode(val);
         return;
     }
-
-    root = searchUtil(root, val);
-
+    root = splay(root, val);
     if(root->value == val)
         return;
 
-    new_node = new SplayNode(val);
-    if(root->value > val)
-    {
-        new_node->right = root;
-        new_node->left = root->left;
-        root->left = NULL;
-    }
-    else
-    {
-        new_node->left = root;
-        new_node->right = root->right;
-        root->right = NULL;
-    }
-    root = new_node;
+    p = split(root, val);
+    root = new SplayNode(val);
+    root->left = p.first;
+    root->right = p.second;
 }
 
 
@@ -200,24 +221,14 @@ void SplayTree::remove(int val)
 {
     SplayNode* rem_node = NULL;
 
-    if(!root)
-        return;
+    if(isEmpty()) return;
 
-    root = searchUtil (root, val);
-
+    root = splay (root, val);
     if(root->value != val)
         return;
 
     rem_node = root;
-    if(!root->left)
-    {
-        root = root->right;
-    }
-    else
-    {
-        root = searchUtil (root->left, val);
-        root->right = rem_node->right;
-    }
+    root = join(root->left, root->right);
     delete rem_node;
     rem_node = NULL;
 }
